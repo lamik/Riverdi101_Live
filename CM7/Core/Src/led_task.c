2d4b23f3a1ee8b6ab78df3212b4db57422e3481f
Mateusz Salamon <mateusz@msalamon.pl>
2022-06-13 13:12:31 +0200

    Basic LED Task.

--- CM7/Core/Src/led_task.c
+++ CM7/Core/Src/led_task.c
@@ -7,10 +7,17 @@
 #include "main.h"
 #include "led_task.h"
 
+#include "FreeRTOS.h"
+#include "cmsis_os.h"
+
+#include "gpio.h"
+
 void LedTaskLoop(void)
 {
 	for(;;)
 	{
+		HAL_GPIO_TogglePin(USR_LED_GPIO_Port, USR_LED_Pin);
+		osDelay(300);
 		// Infinite Loop
 	}
 }
